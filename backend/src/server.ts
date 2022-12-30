eafa41e694f902c05a217720b63dccd5bd11d4a5
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-12-30 18:48:20 -0300

    Server that runs on localhost

---
+++ backend/src/server.ts
@@ -0,0 +1,7 @@
+import { app } from './app';
+
+const port = process.env.PORT ? process.env.PORT : 8080
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+})
