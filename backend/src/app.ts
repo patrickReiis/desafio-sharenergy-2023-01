ef97ebd505d72a30cb9eed321d1b8091aa335eb1
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-12-30 18:58:08 -0300

    Login endpoint with callback

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import * as dotenv from 'dotenv'; 
 dotenv.config();
+import { handleLogin } from './routes/auth/login';
 
 export const app = express();
 app.use(express.json( {limit: '10mb' } ));
 app.use(express.urlencoded({ extended: true }));
+
+app.post('/api/v1/login', handleLogin);
