c95c5dbee4d2c52cfb65d8e5c0fcfc5c34ca0602
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-12-31 15:18:21 -0300

    Authentication route done, if the user is authenticated call the 'next()' function to get the next callback

--- backend/src/routes/auth/authentication.ts
+++ backend/src/routes/auth/authentication.ts
@@ -1,5 +1,32 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { AdminModel } from '../../db/models/AdminModel';
 
-export function isAuthenticated(req: Request, res: Response) {
-    res.end('Hello');
+export async function isAuthenticated(req: Request, res: Response, next: NextFunction) {
+    const { sessionId } = req.cookies;
+
+    if (typeof sessionId !== 'string') {
+        res.status(400).send(); // bad request
+        return
+    }
+
+    const adminUser = await AdminModel.findOne({ 'session.token': sessionId });
+
+    if (adminUser === null) {
+        res.status(400).send(); // user doesn't exist
+        return
+    }
+
+    const cookieExpiresIn = adminUser.session.expiresIn;
+
+    if (isCookieExpired(cookieExpiresIn) === true) {
+        res.status(422).send(); // session is not valid
+        return
+    }
+
+    next();
+}
+
+function isCookieExpired(cookieDate:Date):boolean{
+    const today = new Date();
+    return today > cookieDate;
 }
