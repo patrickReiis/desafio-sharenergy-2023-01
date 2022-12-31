51938c80cf5ab7980371631e3fd9b68e75b9561d
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-12-31 15:18:52 -0300

    Basic users route protected

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import cookieParser from 'cookie-parser';
 import { handleLogin } from './routes/auth/login';
+import { isAuthenticated } from './routes/auth/authentication';
 
 export const app = express();
 app.use(cookieParser());
@@ -11,3 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/api/v1/login', handleLogin);
 
+app.get('/api/v1/users', isAuthenticated, (req, res) => {
+    res.end('This is a protected route');
+}) 
