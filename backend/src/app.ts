d1cd6d9556e150c3d64ba46090c11b73e0bd3c94
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-12-31 10:46:28 -0300

    Using cookie parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import * as dotenv from 'dotenv'; 
 dotenv.config();
+import cookieParser from 'cookie-parser';
 import { handleLogin } from './routes/auth/login';
 
 export const app = express();
+app.use(cookieParser());
 app.use(express.json( {limit: '10mb' } ));
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/api/v1/login', handleLogin);
+
