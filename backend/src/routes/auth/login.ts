620cc1208ea1f5cf2822b4b654c71525c0880207
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-12-30 22:42:39 -0300

    Logging in a user and saving the session in a cookie if everythin goes right

--- backend/src/routes/auth/login.ts
+++ backend/src/routes/auth/login.ts
@@ -1,5 +1,61 @@
+import crypto from 'crypto';
 import { Request, Response } from 'express';
+import { AdminModel, IAdmin } from '../../db/models/AdminModel';
+import { CallbackError } from 'mongoose';
 
 export function handleLogin(req: Request, res: Response) {
-    res.send('Hello, Sun ☀️')
+    const { username, password, remember } = req.body;
+
+    if (typeof username !== 'string' || typeof password !== 'string' || typeof remember !== 'boolean') { 
+        res.status(400).send();
+        return
+    }
+
+    AdminModel.findOneAndUpdate({ username: username, password: password },
+        { session: { 
+            token: getNewSession(), 
+            expiresIn: getExpireDate(remember) 
+        }},
+        {new: true}, 
+        (err, doc) => { 
+            if (err) throw err;
+
+            if (doc === null) {
+                res.writeHead(401); // invalid credentials for login
+                res.end()
+                return
+            }
+
+            console.log('Doc updated. The updated doc is: ', doc);
+
+            res.writeHead(200, {
+                'Set-Cookie': 
+                    [`sessionid=${doc.session.token};HttpOnly;Secure;Expires=${doc.session.expiresIn.toUTCString()};Path=/`]
+            })
+            res.end()
+            return
+        })
+}
+
+function getNewSession(size=40): string {
+    return crypto
+		.randomBytes(size) // getting random bytes
+		.toString('hex') // converting buffer to string with 'hex' decoding
+		.slice(0, size); // slicing the string to fit the size
+}
+
+function  getExpireDate(remember: boolean): Date {
+    // if remember is true the session is valid for 2 years
+    // if remember is false the session is valid for 2 hours
+    const expireDate = new Date();
+
+    if (remember === true) {
+        const twoYearsInHours = 17532;
+        expireDate.setUTCHours(expireDate.getUTCHours() + twoYearsInHours)
+    } else {
+        const threeHours = 3;
+        expireDate.setUTCHours(expireDate.getUTCHours() + threeHours)
+    }
+
+    return expireDate
 }
