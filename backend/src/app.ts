8d3c19edc5a461ae136229c3f3305532a9e73ee5
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-01-01 16:41:56 -0300

    Update user endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import cookieParser from 'cookie-parser';
 import { handleLogin } from './routes/auth/login';
 import { isAuthenticated } from './routes/auth/authentication';
-import { handleGetUsers, handleCreateUser } from './routes/customer/user';
+import { handleGetUsers, handleCreateUser, handleUpdateUser } from './routes/customer/user';
 
 export const app = express();
 app.use(cookieParser());
@@ -16,3 +16,5 @@ app.post('/api/v1/login', handleLogin);
 app.get('/api/v1/users', isAuthenticated, handleGetUsers) 
 
 app.post('/api/v1/user', isAuthenticated, handleCreateUser);
+
+app.put('/api/v1/user/[0-9]+', isAuthenticated, handleUpdateUser);
