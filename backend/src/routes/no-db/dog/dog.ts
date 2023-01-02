af3a7adcd73fee410a1e94ccf7880f86b49ebde6
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-01-02 17:07:26 -0300

    GET random dog endpoint function definition

--- backend/src/routes/no-db/dog/dog.ts
+++ backend/src/routes/no-db/dog/dog.ts
@@ -1,5 +1,24 @@
 import { Request, Response } from 'express';
+import https from 'https';
 
 export async function handleRandomDog(req: Request, res: Response) {
-    res.send('hello from dog');
+    const imagesTypes = 'jpg,png,jpeg';
+    const apiUrl = 'https://random.dog/'; 
+    const urlOptions = `${apiUrl}woof?include=${imagesTypes}`;
+
+    try {
+        https.get(urlOptions, (dogResponse) => {
+            let rawData = '';
+            dogResponse .on('data', (chunk) => rawData += chunk);
+            dogResponse.on('end', () => {
+                const data = rawData;
+                res.status(200).json({ url: apiUrl + data});
+                return
+            })
+        })
+    } catch (e) {
+        console.log('Error during getting random dog image: ', e);
+        res.status(500).send();
+        return
+    }
 }
