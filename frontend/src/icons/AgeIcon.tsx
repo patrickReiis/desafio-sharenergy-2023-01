79f2e08f1faa9785571c34c6cab446d1ca278dc5
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-01-06 10:52:13 -0300

    Icon component done

---
+++ frontend/src/icons/AgeIcon.tsx
@@ -0,0 +1,46 @@
+import { IconProps } from "./IconProps";
+
+function AgeIcon(props: IconProps) {
+  return (
+
+<svg version="1.1"
+  className={props.selectedClass}
+  onClick={props.onClick}
+  id="icon-age"
+  xmlSpace="preserve"
+  width="1078.3052"
+  height="973.69427"
+  viewBox="0 0 1078.3052 973.69427"
+  xmlns="http://www.w3.org/2000/svg"
+  ><defs
+     id="defs6"><clipPath
+       clipPathUnits="userSpaceOnUse"
+       id="clipPath16"><path
+         d="M 0,4.8828125e-4 H 3375 V 3375.0005 H 0 Z"
+         clipRule="evenodd"
+         id="path14" /></clipPath><clipPath
+       clipPathUnits="userSpaceOnUse"
+       id="clipPath28"><path
+         d="M 0,4.8828125e-4 H 3375 V 3375.0005 H 0 Z"
+         clipRule="evenodd"
+         id="path26" /></clipPath><clipPath
+       clipPathUnits="userSpaceOnUse"
+       id="clipPath40"><path
+         d="M 0,165.68213 H 3375 V 3209.4321 H 0 Z"
+         clipRule="evenodd"
+         id="path38" /></clipPath></defs><g
+     id="g8"
+     transform="matrix(0.31999998,0,0,0.31999998,-0.84737563,-53.170989)"><g
+       id="g34"><g
+         id="g36"
+         clipPath="url(#clipPath40)"><g
+           id="g42"
+           transform="matrix(108.70012,0,0,108.70537,-51.702011,-51.728607)"><path
+             d="M 26,4.46631 H 24.51318 V 3.50439 c 0,-0.8286099 -0.671391,-1.5 -1.5,-1.5 -0.82861,0 -1.5,0.6713901 -1.5,1.5 V 4.4663 H 17.5 V 3.50439 c 0,-0.8286099 -0.671391,-1.5 -1.5,-1.5 -0.82861,0 -1.5,0.6713901 -1.5,1.5 V 4.4663 H 10.48682 V 3.50439 c 0,-0.8286099 -0.6713894,-1.5 -1.4999998,-1.5 -0.8286104,0 -1.5,0.6713901 -1.5,1.5 V 4.4663 H 6 c -3.0327101,0 -5.5,2.4672899 -5.5,5.5 v 14.5293 c 0,3.032709 2.4672899,5.5 5.5,5.5 h 20 c 3.032711,0 5.5,-2.467291 5.5,-5.5 V 9.9663105 C 31.5,6.9335899 29.032709,4.46631 26,4.46631 Z m -20,3 h 1.4868202 v 0.9130903 c 0,0.8286104 0.6713896,1.5 1.5,1.5 0.8286104,0 1.4999998,-0.6713896 1.4999998,-1.5 V 7.46631 H 14.5 v 0.9130903 c 0,0.8286104 0.67139,1.5 1.5,1.5 0.828609,0 1.5,-0.6713896 1.5,-1.5 V 7.46631 h 4.01318 v 0.9130903 c 0,0.8286104 0.67139,1.5 1.5,1.5 0.828609,0 1.5,-0.6713896 1.5,-1.5 V 7.46631 H 26 c 1.37842,0 2.5,1.1215797 2.5,2.5000005 V 11.62207 H 3.5 V 9.9663105 C 3.5,8.5878897 4.6215801,7.46631 6,7.46631 Z M 26,26.995609 H 6 c -1.3784199,0 -2.5,-1.12158 -2.5,-2.5 v -9.87354 h 25 v 9.87354 c 0,1.378411 -1.12158,2.5 -2.5,2.5 z"
+
+             style={{fill:'inherit',fillOpacity:'1',fillRule:'nonzero',stroke:'none'}}
+             id="path44" /></g></g></g></g></svg>
+  )
+}
+
+export default AgeIcon
